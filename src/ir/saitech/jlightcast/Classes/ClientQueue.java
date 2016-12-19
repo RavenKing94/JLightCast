4619c2e8e38676626271b673467a4b7b0f264291
RavenKing94 <kasra.sh13@gmail.com>
2016-12-19 21:25:26 +0330

    class ClientQueue first implementation

--- src/ir/saitech/jlightcast/Classes/ClientQueue.java
+++ src/ir/saitech/jlightcast/Classes/ClientQueue.java
@@ -1,16 +1,45 @@
 package ir.saitech.jlightcast.Classes;
 
-import java.util.concurrent.SynchronousQueue;
+import sun.awt.Mutex;
+
+import java.util.Objects;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by blk-arch on 12/14/16.
  *
  */
 public class ClientQueue {
-    private static SynchronousQueue<ClientSocket> clq = new SynchronousQueue<>(false);
-    private static boolean lock=false;
+    private static Queue<ClientSocket> queue = new ArrayBlockingQueue<>(20);
+    private static Lock lock = new ReentrantLock(true);
 
-    private static void add(ClientSocket cls){
+    public static void add(ClientSocket cls){
+        lock.lock();
+        queue.add(cls);
+        lock.unlock();
+    }
 
+    public static ClientSocket get(){
+        lock.lock();
+        ClientSocket c;
+        c = queue.poll();
+        lock.unlock();
+        return c;
+    }
+
+    public static ClientSocket[] getAll(){
+        lock.lock();
+        ClientSocket[] cl;
+        Object[] ol;
+        ol = queue.toArray();
+        cl = new ClientSocket[ol.length];
+        for (int i=0;i<ol.length;i++){
+            cl[i] = (ClientSocket)ol[i];
+        }
+        lock.unlock();
+        return cl;
     }
 }
